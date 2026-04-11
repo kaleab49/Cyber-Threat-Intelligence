008c213413f983485bee8b6e3f19c0c0f458b937
kaleab yemane <kaleaby49@gmail.com>
2026-04-11 22:00:03 +0300

    Update Routing

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,8 +2,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return HttpResponse("<h1>Please Head to /api</h1>")
 
-
-def about(request):
-    return HttpResponse("<h2>Someone is here</h2>")
